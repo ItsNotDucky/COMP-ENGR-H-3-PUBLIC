a49a0884f88c9c4f488f1e8f3ed8b4aba6a4b349
MOhammeds124 <147172468+MOhammeds124@users.noreply.github.com>
2024-10-11 10:04:04 -0400

    Add files via upload

---
+++ html-miniproject-graddingreport/public/script.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", addlistener)
+
+function addlistener()
+{
+	document.getElementById("btn1").addEventListener("click",disapear)
+}
+
+function disapear()
+{
+	
+	checkboxes()
+}
